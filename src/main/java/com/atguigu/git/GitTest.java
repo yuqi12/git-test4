1402baccc68b6e3d8b74e4e8f961145cd2fcd82e
yuqi <yuqi@yushuhan.com>
2022-04-16 22:26:13 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author yuqi
+ * @create 2022-04-16 22:20
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
